b2a2b480bed00627ec5c42c2b39350b98719d4ea
tanchangsong <2781427060@qq.com>
2021-02-13 16:39:49 +0800

    初始化vue项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+   {{addResult}}
+
+   <p v-if="seen">你好，效力</p>
+   <!--
+   <div id="dooor-test">
+     <ol>
+      <li v-for="todo in todos">
+        {{ todo.text }}
+      </li>
+    </ol>
+  </div> -->
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      addResult: 'Welcome to China'+ new Date().toLocaleString(),
+      seen: true,
+      todos: [
+      { text: '学习 JavaScript' },
+      { text: '学习 Vue' },
+      { text: '整个牛项目' }
+    ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
